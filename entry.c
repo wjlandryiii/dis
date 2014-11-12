ed2cf18ae74c42ab3aa1ad41d7c6d211254012b0
wjlandryiii <wjlandryiii@gmail.com>
2014-11-11 22:14:48 -0800

    added entry.c but filename might chaange

---
+++ entry.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2014 Joseph Landry
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
---
+++ entry.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2014 Joseph Landry
+ */
+
+#ifndef ENTRY_H
+#define ENTRY_H
+
+struct entrypoints {
+
+};
+
+struct entrypoints *new_entrypoints(void);
+void free_entrypoints(struct entrypoints *ep);
+
+
+
+#endif
